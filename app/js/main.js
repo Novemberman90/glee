e4b882ad5d60e156848c33727ea10ae40a172aba
Alexey <alexledovit@gmail.com>
2024-06-18 20:23:12 +0300

    add adaptive detalis page

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,29 @@ $(function() {
         prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 52 52"><rect x="1" y="1" width="50" height="50" rx="5" ry="5"/><path d="M1460.7,1330.53h-16.94l0.1-2.86a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-0.18-.17a0.267,0.267,0,0,0-.4,0l-4.2,4.21a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l4.2,4.21a0.29,0.29,0,0,0,.4,0l0.18-.17a0.323,0.323,0,0,0,.08-0.21,0.3,0.3,0,0,0-.08-0.2l-0.12-2.86h16.97a0.3,0.3,0,0,0,.29-0.3v-1.37A0.291,0.291,0,0,0,1460.7,1330.53Z" transform="translate(-1424 -1305)"/></svg></button>',
         nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 52 52"><rect x="1" y="1" width="50" height="50" rx="5" ry="5"/><path d="M1509.3,1332.47h16.94l-0.1,2.86a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l0.18,0.17a0.267,0.267,0,0,0,.4,0l4.2-4.21a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-4.2-4.21a0.29,0.29,0,0,0-.4,0l-0.18.17a0.323,0.323,0,0,0-.08.21,0.318,0.318,0,0,0,.08.2l0.12,2.86h-16.97a0.3,0.3,0,0,0-.29.3v1.37A0.291,0.291,0,0,0,1509.3,1332.47Z" transform="translate(-1494 -1305)"/></svg></button>',
          infinite: false,
+        responsive: [
+        {
+          breakpoint: 1061,
+          settings: {
+             slidesToShow: 3,
+             slidesToScroll: 3,
+            draggable: true,
+          },
+          breakpoint: 861,
+          settings: {
+             slidesToShow: 2,
+             slidesToScroll: 2,
+            draggable: true,
+          },
+          breakpoint: 601,
+          settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+            draggable: true,
+          },
+
+        }
+      ],
     });
 
 
@@ -44,6 +67,14 @@ $(function() {
       draggable: false,
       arrows: false,
       fade: true,
+      responsive: [
+        {
+          breakpoint: 1021,
+          settings: {
+            draggable: true,
+          }
+        }
+      ],
     });
 
     $('.product-item-list__btn-card').on('click', function() {
